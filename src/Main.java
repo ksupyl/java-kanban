a5aa56e785c23b1e38c4e99066a6c897997db6fd
Ksenia <Xeonica01owl@gmail.com>
2025-11-23 00:16:04 +0300

    Class Main: creating two tasks, as well as an epic with two subtasks and an epic with one subtask.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,34 @@
 import model.Status;
 import model.Task;
+import model.Epic;
+import model.Subtask;
+import service.TaskManager;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Поехали!");
+        TaskManager manager = new TaskManager();
+
+        // Создание двух Задач
+        Task task1 = new Task("Задача 1", "Описание 1 Задачи", Status.NEW);
+        Task task2 = new Task("Задача 2", "Описание 2 Задачи", Status.NEW);
+        manager.createTask(task1);
+        manager.createTask(task2);
+
+        // Создание Эпика с двумя Подзадачами
+        Epic epic1 = new Epic("Эпик 1", "Эпик с 2 подзадачами");
+        manager.createEpic(epic1);
+
+        Subtask subtask1 = new Subtask("Подзадача 1", "Подзадача Эпика 1", Status.NEW, epic1.getId());
+        Subtask subtask2 = new Subtask("Подзадача 2", "Подзадача Эпика 1", Status.NEW, epic1.getId());
+        manager.createSubtask(subtask1);
+        manager.createSubtask(subtask2);
+
+        // Создание Эпика с одной подзадачей
+        Epic epic2 = new Epic("Эпик 2", "Эпик с 1 подзадачей");
+        manager.createEpic(epic2);
+
+        Subtask subtask3 = new Subtask("Подзадача 3", "Подзадача Эпика 2", Status.NEW, epic2.getId());
+        manager.createSubtask(subtask3);
     }
 }
