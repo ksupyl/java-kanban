e3612ae8a7a1560adcf15c011939b122c9734c3f
Ksenia <Xeonica01owl@gmail.com>
2026-03-18 19:32:51 +0300

    style: fix CheckStyle violations in Node.java

--- src/service/Node.java
+++ src/service/Node.java
@@ -12,11 +12,23 @@ public class Node {
         this.task = task;
     }
 
-    public Task getTask() { return task; }
+    public Task getTask() {
+        return task;
+    }
 
-    public Node getPrev() { return prev; }
-    public void setPrev(Node prev) { this.prev = prev; }
+    public Node getPrev() {
+        return prev;
+    }
 
-    public Node getNext() { return next; }
-    public void setNext(Node next) { this.next = next; }
+    public void setPrev(Node prev) {
+        this.prev = prev;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
