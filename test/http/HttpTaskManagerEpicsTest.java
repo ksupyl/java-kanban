c124b81c36f9474403494802516ebc5a10810b92
Ksenia <Xeonica01owl@gmail.com>
2026-04-18 19:14:25 +0300

    test: add HTTP tests for epics and subtasks endpoints

---
+++ test/http/HttpTaskManagerEpicsTest.java
@@ -0,0 +1,170 @@
+package http;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import model.Epic;
+import model.Subtask;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+// Тесты для endpoint'а /epics.
+public class HttpTaskManagerEpicsTest extends HttpTaskServerTestBase {
+
+    // Проверка: GET /epics должен возвращать пустой список,
+    // если эпиков в менеджере нет.
+    @Test
+    public void shouldReturnEmptyEpicsList() throws IOException, InterruptedException {
+        URI url = URI.create("http://localhost:8080/epics");
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(url)
+                .GET()
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        assertEquals(200, response.statusCode(), "Некорректный код ответа.");
+        assertEquals("[]", response.body(), "Список эпиков должен быть пустым.");
+    }
+
+    // Проверка: POST /epics должен создавать новый эпик.
+    @Test
+    public void shouldCreateEpic() throws IOException, InterruptedException {
+        Epic epic = createTestEpic("Покупки", "Список покупок на вечер");
+        String epicJson = gson.toJson(epic);
+
+        URI url = URI.create("http://localhost:8080/epics");
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(url)
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(epicJson))
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        List<Epic> epicsFromManager = manager.getEpics();
+
+        assertEquals(201, response.statusCode(), "Некорректный код ответа при создании эпика.");
+        assertNotNull(epicsFromManager, "Список эпиков не должен быть null.");
+        assertEquals(1, epicsFromManager.size(), "Некорректное количество эпиков.");
+        assertEquals("Покупки", epicsFromManager.get(0).getName(), "Некорректное имя эпика.");
+    }
+
+    // Проверка: GET /epics/{id} должен возвращать эпик по id.
+    @Test
+    public void shouldReturnEpicById() throws IOException, InterruptedException {
+        Epic epic = createTestEpic("Учёба", "Подготовка к экзамену");
+        manager.createEpic(epic);
+
+        int epicId = manager.getEpics().get(0).getId();
+
+        URI url = URI.create("http://localhost:8080/epics/" + epicId);
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(url)
+                .GET()
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        Epic epicFromResponse = gson.fromJson(response.body(), Epic.class);
+
+        assertEquals(200, response.statusCode(), "Некорректный код ответа при получении эпика по id.");
+        assertNotNull(epicFromResponse, "Эпик не пришёл в ответе.");
+        assertEquals(epicId, epicFromResponse.getId(), "Некорректный id эпика.");
+        assertEquals("Учёба", epicFromResponse.getName(), "Некорректное имя эпика.");
+    }
+
+    // Проверка: GET /epics/{id} должен возвращать 404,
+    // если эпика с таким id не существует.
+    @Test
+    public void shouldReturn404WhenEpicNotFound() throws IOException, InterruptedException {
+        URI url = URI.create("http://localhost:8080/epics/999");
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(url)
+                .GET()
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        assertEquals(404, response.statusCode(), "Должен вернуться статус 404, если эпик не найден.");
+    }
+
+    // Проверка: GET /epics/{id}/subtasks должен возвращать список подзадач эпика.
+    @Test
+    public void shouldReturnEpicSubtasks() throws IOException, InterruptedException {
+        Epic epic = createTestEpic("Переезд", "Подготовка к переезду");
+        manager.createEpic(epic);
+
+        int epicId = manager.getEpics().get(0).getId();
+
+        Subtask subtask = createTestSubtask(
+                "Собрать коробки",
+                "Упаковать вещи",
+                60,
+                LocalDateTime.of(2026, 4, 22, 10, 0),
+                epicId
+        );
+        manager.createSubtask(subtask);
+
+        URI url = URI.create("http://localhost:8080/epics/" + epicId + "/subtasks");
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(url)
+                .GET()
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        JsonArray jsonArray = JsonParser.parseString(response.body()).getAsJsonArray();
+        JsonObject firstSubtask = jsonArray.get(0).getAsJsonObject();
+
+        assertEquals(200, response.statusCode(),
+                "Некорректный код ответа при получении подзадач эпика.");
+        assertEquals(1, jsonArray.size(), "У эпика должна быть одна подзадача.");
+        assertEquals("Собрать коробки", firstSubtask.get("name").getAsString(),
+                "Некорректное имя подзадачи в ответе.");
+    }
+
+    // Проверка: GET /epics/{id}/subtasks должен возвращать 404,
+    // если эпика с таким id не существует.
+    @Test
+    public void shouldReturn404WhenEpicSubtasksRequestedForMissingEpic() throws IOException, InterruptedException {
+        URI url = URI.create("http://localhost:8080/epics/999/subtasks");
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(url)
+                .GET()
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        assertEquals(404, response.statusCode(),
+                "Должен вернуться статус 404, если подзадачи запрашиваются у несуществующего эпика.");
+    }
+
+    // Проверка: DELETE /epics/{id} должен удалять эпик.
+    @Test
+    public void shouldDeleteEpicById() throws IOException, InterruptedException {
+        Epic epic = createTestEpic("Отпуск", "Подготовка к отпуску");
+        manager.createEpic(epic);
+
+        int epicId = manager.getEpics().get(0).getId();
+
+        URI url = URI.create("http://localhost:8080/epics/" + epicId);
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(url)
+                .DELETE()
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        assertEquals(200, response.statusCode(), "Некорректный код ответа при удалении эпика.");
+        assertTrue(manager.getEpics().isEmpty(), "Эпик не был удалён из менеджера.");
+    }
+}
---
+++ test/http/HttpTaskManagerSubtasksTest.java
@@ -0,0 +1,221 @@
+package http;
+
+import model.Epic;
+import model.Subtask;
+import model.Task;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+// Тесты для endpoint'а /subtasks.
+public class HttpTaskManagerSubtasksTest extends HttpTaskServerTestBase {
+
+    // Проверка: GET /subtasks должен возвращать пустой список,
+    // если подзадач в менеджере нет.
+    @Test
+    public void shouldReturnEmptySubtasksList() throws IOException, InterruptedException {
+        URI url = URI.create("http://localhost:8080/subtasks");
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(url)
+                .GET()
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        assertEquals(200, response.statusCode(), "Некорректный код ответа.");
+        assertEquals("[]", response.body(), "Список подзадач должен быть пустым.");
+    }
+
+    // Проверка: POST /subtasks должен создавать новую подзадачу,
+    // если эпик существует.
+    @Test
+    public void shouldCreateSubtask() throws IOException, InterruptedException {
+        Epic epic = createTestEpic("Домашние дела", "Список домашних дел");
+        manager.createEpic(epic);
+
+        int epicId = manager.getEpics().get(0).getId();
+
+        Subtask subtask = createTestSubtask(
+                "Помыть окна",
+                "На кухне и в комнате",
+                45,
+                LocalDateTime.of(2026, 4, 23, 14, 0),
+                epicId
+        );
+
+        String subtaskJson = gson.toJson(subtask);
+
+        URI url = URI.create("http://localhost:8080/subtasks");
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(url)
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(subtaskJson))
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        List<Subtask> subtasksFromManager = manager.getSubtasks();
+
+        assertEquals(201, response.statusCode(), "Некорректный код ответа при создании подзадачи.");
+        assertNotNull(subtasksFromManager, "Список подзадач не должен быть null.");
+        assertEquals(1, subtasksFromManager.size(), "Некорректное количество подзадач.");
+        assertEquals("Помыть окна", subtasksFromManager.get(0).getName(), "Некорректное имя подзадачи.");
+    }
+
+    // Проверка: GET /subtasks/{id} должен возвращать подзадачу по id.
+    @Test
+    public void shouldReturnSubtaskById() throws IOException, InterruptedException {
+        Epic epic = createTestEpic("Учёба", "Подготовка к занятиям");
+        manager.createEpic(epic);
+
+        int epicId = manager.getEpics().get(0).getId();
+
+        Subtask subtask = createTestSubtask(
+                "Решить задачи",
+                "Практика по Java",
+                90,
+                LocalDateTime.of(2026, 4, 23, 18, 0),
+                epicId
+        );
+        manager.createSubtask(subtask);
+
+        int subtaskId = manager.getSubtasks().get(0).getId();
+
+        URI url = URI.create("http://localhost:8080/subtasks/" + subtaskId);
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(url)
+                .GET()
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        Subtask subtaskFromResponse = gson.fromJson(response.body(), Subtask.class);
+
+        assertEquals(200, response.statusCode(), "Некорректный код ответа при получении подзадачи по id.");
+        assertNotNull(subtaskFromResponse, "Подзадача не пришла в ответе.");
+        assertEquals(subtaskId, subtaskFromResponse.getId(), "Некорректный id подзадачи.");
+        assertEquals("Решить задачи", subtaskFromResponse.getName(), "Некорректное имя подзадачи.");
+    }
+
+    // Проверка: GET /subtasks/{id} должен возвращать 404,
+    // если подзадачи с таким id не существует.
+    @Test
+    public void shouldReturn404WhenSubtaskNotFound() throws IOException, InterruptedException {
+        URI url = URI.create("http://localhost:8080/subtasks/999");
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(url)
+                .GET()
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        assertEquals(404, response.statusCode(), "Должен вернуться статус 404, если подзадача не найдена.");
+    }
+
+    // Проверка: POST /subtasks должен возвращать 404,
+    // если у подзадачи указан несуществующий epicId.
+    @Test
+    public void shouldReturn404WhenCreatingSubtaskWithoutExistingEpic() throws IOException, InterruptedException {
+        Subtask subtask = createTestSubtask(
+                "Несуществующий эпик",
+                "Подзадача не должна создаться",
+                30,
+                LocalDateTime.of(2026, 4, 24, 10, 0),
+                999
+        );
+
+        String subtaskJson = gson.toJson(subtask);
+
+        URI url = URI.create("http://localhost:8080/subtasks");
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(url)
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(subtaskJson))
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        assertEquals(404, response.statusCode(),
+                "Должен вернуться статус 404, если подзадача создаётся без существующего эпика.");
+        assertTrue(manager.getSubtasks().isEmpty(), "Подзадача не должна сохраниться в менеджере.");
+    }
+
+    // Проверка: POST /subtasks должен возвращать 406,
+    // если новая подзадача пересекается по времени с существующей задачей.
+    @Test
+    public void shouldReturn406WhenSubtaskHasTimeOverlap() throws IOException, InterruptedException {
+        Epic epic = createTestEpic("Работа", "Рабочие задачи");
+        manager.createEpic(epic);
+
+        int epicId = manager.getEpics().get(0).getId();
+
+        Task task = createTestTask(
+                "Основная задача",
+                "Занимает время",
+                60,
+                LocalDateTime.of(2026, 4, 24, 12, 0)
+        );
+        manager.createTask(task);
+
+        Subtask overlappingSubtask = createTestSubtask(
+                "Пересекающаяся подзадача",
+                "Не должна сохраниться",
+                30,
+                LocalDateTime.of(2026, 4, 24, 12, 30),
+                epicId
+        );
+
+        String subtaskJson = gson.toJson(overlappingSubtask);
+
+        URI url = URI.create("http://localhost:8080/subtasks");
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(url)
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(subtaskJson))
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        assertEquals(406, response.statusCode(),
+                "Должен вернуться статус 406 при пересечении подзадачи по времени.");
+        assertTrue(manager.getSubtasks().isEmpty(), "Пересекающаяся подзадача не должна сохраниться.");
+    }
+
+    // Проверка: DELETE /subtasks/{id} должен удалять подзадачу.
+    @Test
+    public void shouldDeleteSubtaskById() throws IOException, InterruptedException {
+        Epic epic = createTestEpic("Проект", "Работа над проектом");
+        manager.createEpic(epic);
+
+        int epicId = manager.getEpics().get(0).getId();
+
+        Subtask subtask = createTestSubtask(
+                "Написать код",
+                "Основной модуль",
+                120,
+                LocalDateTime.of(2026, 4, 25, 11, 0),
+                epicId
+        );
+        manager.createSubtask(subtask);
+
+        int subtaskId = manager.getSubtasks().get(0).getId();
+
+        URI url = URI.create("http://localhost:8080/subtasks/" + subtaskId);
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(url)
+                .DELETE()
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        assertEquals(200, response.statusCode(), "Некорректный код ответа при удалении подзадачи.");
+        assertTrue(manager.getSubtasks().isEmpty(), "Подзадача не была удалена из менеджера.");
+    }
+}
